1764b026e920977e23cdad6fd2f1b808eb375e37
Cameron Smith <Cs11@usf.edu>
2024-10-06 23:09:39 -0400

    Create Employee class with properties and methods

---
+++ employee_management.js
@@ -0,0 +1,14 @@
+// Task 1: Create an Employee Class
+
+class Employee {
+    constructor(Alice, 85000, Developer, Engineering) {
+        this.name = alice;
+        this.salary = 85000;
+        this.department = Engineering;
+        this.postition = Developer;
+    
+}
+getdetails () {
+    return `$(this.name)` is at `$(this.postion)` and has `$(this.salary)`
+
+}
